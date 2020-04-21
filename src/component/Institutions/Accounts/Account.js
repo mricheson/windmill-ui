58ee31146ec4de487b716abf1c492231c0d83cc7
Mitchell Richeson <mricheson@gmail.com>
2020-04-20 23:35:54 -0400

    fix account modal closing

--- src/component/Institutions/Accounts/Account.js
+++ src/component/Institutions/Accounts/Account.js
@@ -9,20 +9,28 @@ const useStyle = makeStyles({
         textDecoration: 'line-through'
     }
 });
+
 const Account = observer(({ account }) => {
     const classes = useStyle();
     const [modal, setModal] = useState(null);
 
-    const openModal = () => setModal(<AccountModal account={account} onClose={() => setModal(null)} />);
+    const openModal = () => setModal(
+        <AccountModal
+            account={account}
+            onClose={() => setModal(null)}
+        />
+    );
 
     return (
-        <ListItem button key={account.id} onClick={openModal}>
-            <ListItemIcon>
-                <AccountIcon accountType={account.type.type} />
-            </ListItemIcon>
-            <ListItemText primary={account.name} className={account.isActive ? null : classes.inActive}/>
+        <>
+            <ListItem button key={account.id} onClick={openModal}>
+                <ListItemIcon>
+                    <AccountIcon accountType={account.type.type} />
+                </ListItemIcon>
+                <ListItemText primary={account.name} className={account.isActive ? null : classes.inActive} />
+            </ListItem>
             {modal}
-        </ListItem>
+        </>
     );
 });
 
