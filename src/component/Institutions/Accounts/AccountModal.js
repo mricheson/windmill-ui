5f3aa374be6d6ee270d9e09642398743cf5c1dc2
Mitchell Richeson <mricheson@gmail.com>
2020-04-21 00:05:20 -0400

    edit account type

--- src/component/Institutions/Accounts/AccountModal.js
+++ src/component/Institutions/Accounts/AccountModal.js
@@ -1,21 +1,31 @@
 import React, { useContext, useState } from 'react';
-import { TextField, FormControlLabel, Checkbox } from '@material-ui/core';
+import { TextField, FormControlLabel, Checkbox, MenuItem, FormControl, InputLabel, Select } from '@material-ui/core';
 import { observer } from 'mobx-react-lite';
 import { RootStoreContext } from '../../../store/RootStore';
 import EditModal from '../../../common/component/EditModal';
+import { AccountTypeStoreContext } from '../../../store/AccountTypeStore';
 
 const AccountModal = observer(({ account, onClose, onSave = () => { } }) => {
     const rootStore = useContext(RootStoreContext);
+    const accountTypeStore = useContext(AccountTypeStoreContext);
     const [editedName, setEditedName] = useState(account.name);
-    const [editedType, setEditedType] = useState(account.type);
+    const [editedType, setEditedType] = useState(account.type.id);
     const [editedIsActive, setEditedIsActive] = useState(account.isActive);
 
     const onNameChange = event => setEditedName(event.target.value);
 
     const onIsActiveChange = event => setEditedIsActive(event.target.checked);
 
+    const onTypeChange = event => setEditedType(event.target.value);
+
     const save = () => {
-        account.save({ name: editedName, isActive: editedIsActive })
+        account.save({
+            name: editedName,
+            isActive: editedIsActive,
+            type: editedType
+                ? accountTypeStore.accountTypes.find(type => type.id === editedType)
+                : undefined
+        })
             .then(() => onSave(account))
             .then(onClose);
     }
@@ -32,6 +42,17 @@ const AccountModal = observer(({ account, onClose, onSave = () => { } }) => {
                 value={editedName}
                 disabled={isSaving}
             />
+            <FormControl>
+                <InputLabel shrink>
+                    Type
+                </InputLabel>
+                <Select
+                    value={editedType}
+                    onChange={onTypeChange}
+                >
+                    {accountTypeStore.accountTypes.map(type => <MenuItem key={type.id} value={type.id}>{type.type}</MenuItem>)}
+                </Select>
+            </FormControl>
             <FormControlLabel
                 control={
                     <Checkbox
