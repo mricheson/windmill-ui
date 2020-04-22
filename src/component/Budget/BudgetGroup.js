91b996467d8dd24a85c1073ab4f2cdf417790cf0
Mitchell Richeson <mricheson@gmail.com>
2020-04-21 20:35:26 -0400

    Budget Group Card complete...ready for the templates

--- src/component/Budget/BudgetGroup.js
+++ src/component/Budget/BudgetGroup.js
@@ -1,15 +1,18 @@
-import React, { useContext, useState, useEffect } from 'react';
+import React, { useContext, useState } from 'react';
 import { observer } from 'mobx-react-lite';
-import { BudgetCategoryStoreContext } from '../../store/BudgetCategoryStore';
-import { ExpansionPanel, ExpansionPanelSummary, ExpansionPanelDetails, makeStyles, IconButton } from '@material-ui/core';
+import { ExpansionPanel, ExpansionPanelSummary, ExpansionPanelDetails, makeStyles, IconButton, ExpansionPanelActions } from '@material-ui/core';
 import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
 import BudgetGroupModal from './BudgetGroupModal';
 import AccountIcon from '../Institutions/Accounts/AccountIcon';
 import EditIcon from '@material-ui/icons/Edit';
+import { BudgetTemplateStoreContext } from '../../store/BudgetTemplateStore';
+import AddIcon from '@material-ui/icons/Add';
 
 const useStyles = makeStyles(theme => ({
     summary: {
-        display: 'flex'
+        display: 'flex',
+        justifyContent: 'space-between',
+        width: '100%'
     },
     column: {
         display: 'flex',
@@ -18,13 +21,21 @@ const useStyles = makeStyles(theme => ({
     title: {
         display: 'flex',
         alignItems: 'center',
-        margin: [[0,theme.spacing(2),0,theme.spacing(2)]]
+        margin: [[0, theme.spacing(2), 0, theme.spacing(2)]]
+    },
+    templates: {
+        display: 'flex',
+        flexDirection: 'column'
+    },
+    actions: {
+        justifyContent: 'center'
     }
 }));
 
 const BudgetGroup = observer(({ budgetGroup }) => {
     const classes = useStyles();
     const [groupModal, setGroupModal] = useState(null);
+    const budgetTemplateStore = useContext(BudgetTemplateStoreContext);
 
     const openGroupModal = () => setGroupModal(
         <BudgetGroupModal
@@ -33,28 +44,41 @@ const BudgetGroup = observer(({ budgetGroup }) => {
         />
     );
 
+    const templatesForThisGroup = budgetTemplateStore.budgetTemplates.filter(template => template.category.budgetGroup.id === budgetGroup.id);
+    const renderedTemplates = templatesForThisGroup.map(template => <div>{template.name}</div>);
+    const totalAmount = templatesForThisGroup.reduce((total, template) => total + template.amount, 0.0);
+
     return (
         <>
             <ExpansionPanel>
                 <ExpansionPanelSummary
                     expandIcon={<ExpandMoreIcon />}
-                    className={classes.summary}
                 >
-                    <div className={classes.column}>
-                    <AccountIcon accountType={budgetGroup.isSavings ? 'savings' : 'expense'} />
-                    <div className={classes.title}>
-                        {budgetGroup.name}
-                    </div>
-                    <IconButton onClick={openGroupModal}>
-                        <EditIcon color="disabled"  fontSize="small"/>
-                    </IconButton>
-                    </div>
-                    <div className={classes.column}>
-
+                    <div className={classes.summary}>
+                        <div className={classes.column}>
+                            <AccountIcon accountType={budgetGroup.isSavings ? 'savings' : 'expense'} />
+                            <div className={classes.title}>
+                                {budgetGroup.name}
+                            </div>
+                            <IconButton onClick={openGroupModal}>
+                                <EditIcon color="disabled" fontSize="small" />
+                            </IconButton>
+                        </div>
+                        <div className={classes.column}>
+                            {`$${totalAmount.toFixed(2)}`}
+                        </div>
                     </div>
                 </ExpansionPanelSummary>
                 <ExpansionPanelDetails>
+                    <div classNamec={classes.templates}>
+                        {renderedTemplates}
+                    </div>
                 </ExpansionPanelDetails>
+                <ExpansionPanelActions className={classes.actions} >
+                    <IconButton onClick={() => { }}>
+                        <AddIcon />
+                    </IconButton>
+                </ExpansionPanelActions>
             </ExpansionPanel>
             {groupModal}
         </>
