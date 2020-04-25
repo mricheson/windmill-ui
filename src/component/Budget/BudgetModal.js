1b500cb6a9050506e4717f95d65088bf55277703
Mitchell Richeson <mricheson@gmail.com>
2020-04-25 15:01:33 -0400

    Budget Modal Title

--- src/component/Budget/BudgetModal.js
+++ src/component/Budget/BudgetModal.js
@@ -1,12 +1,15 @@
 import React, { useContext } from 'react';
-import { makeStyles, Dialog, AppBar, Toolbar, IconButton, Slide } from '@material-ui/core';
+import { makeStyles, Dialog, AppBar, Toolbar, IconButton, Slide, Typography } from '@material-ui/core';
 import { observer } from 'mobx-react-lite';
 import { RootStoreContext } from '../../store/RootStore';
 import CloseIcon from '@material-ui/icons/Close';
+import moment from 'moment';
 
-const useStyles = makeStyles({
-
-});
+const useStyles = makeStyles(theme => ({
+    close: {
+        marginRight: theme.spacing(2),
+    }
+}));
 
 const Transition = React.forwardRef(function Transition(props, ref) {
     return <Slide direction="up" ref={ref} {...props} />;
@@ -20,10 +23,12 @@ const BudgetModal = observer(({ budget, onClose }) => {
         <Dialog fullScreen open onClose={onClose} TransitionComponent={Transition}>
             <AppBar className={classes.appBar}>
                 <Toolbar>
-                    <IconButton edge="start" color="inherit" onClick={onClose} aria-label="close">
+                    <IconButton edge="start" color="inherit" onClick={onClose} aria-label="close" className={classes.close}>
                         <CloseIcon />
                     </IconButton>
-                        Sound
+                    <Typography variant="h6" >
+                        {moment(budget.date).format('MMMM YYYY')}
+                    </Typography>
                 </Toolbar>
             </AppBar>
             {budget.date}
