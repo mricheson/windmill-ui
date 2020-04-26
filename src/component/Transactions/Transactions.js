13d2f177e03c96cfcf97c40d7b3ddb86f7c4aab0
Mitchell Richeson <mricheson@gmail.com>
2020-04-25 20:07:28 -0400

    Transaction Table Refining

--- src/component/Transactions/Transactions.js
+++ src/component/Transactions/Transactions.js
@@ -4,6 +4,9 @@ import { TransactionStoreContext } from '../../store/TransactionStore';
 import { TableContainer, Table, makeStyles, TableHead, TableRow, TableCell, TableBody } from '@material-ui/core';
 import Paper from '@material-ui/core/Paper';
 import { observer } from 'mobx-react-lite';
+import TransactionIcon from './TransactionIcon';
+import EditIcon from '@material-ui/icons/Edit';
+import moment from 'moment';
 
 const useStyles = makeStyles(theme => ({
     root: {
@@ -13,6 +16,27 @@ const useStyles = makeStyles(theme => ({
         alignItems: 'center'
     }
 }))
+
+const getType = transaction => {
+    if (transaction.payment) {
+        return 'payment';
+    }
+
+    if (transaction.reimbursable) {
+        return 'reimbursable';
+    }
+
+    if (transaction.reimbursement) {
+        return 'reimbursement';
+    }
+
+    return '';
+}
+
+const formatter = new Intl.NumberFormat('en-US', {
+    style: 'currency',
+    currency: 'USD',
+});
 
 const Transactions = observer(() => {
     const classes = useStyles();
@@ -27,34 +51,30 @@ const Transactions = observer(() => {
         <div className={classes.root}>
             <div>{`Transactions ${year}-${month}`}</div>
             <TableContainer component={Paper}>
-                <Table className={classes.table}>
+                <Table className={classes.table} size="small">
                     <TableHead>
                         <TableRow>
                             <TableCell>Account</TableCell>
-                            <TableCell>Trans Date</TableCell>
-                            <TableCell>Post Date</TableCell>
+                            <TableCell>Date</TableCell>
                             <TableCell>Description</TableCell>
                             <TableCell>Amount</TableCell>
                             <TableCell>Category</TableCell>
-                            <TableCell>Reimb Exp</TableCell>
-                            <TableCell>Reimb</TableCell>
-                            <TableCell>Payment</TableCell>
                             <TableCell>Comment</TableCell>
+                            <TableCell></TableCell>
+                            <TableCell></TableCell>
                         </TableRow>
                     </TableHead>
                     <TableBody>
-                        {transactionStore.transactions.map(transaction => (
+                        {transactionStore.transactions.slice().sort((a, b) => new Date(a.transactionDate) - new Date(b.transactionDate)).map(transaction => (
                             <TableRow key={transaction.id}>
-                                <TableCell>{`${transaction.account.institution.name}::${transaction.account.account}`}</TableCell>
-                                <TableCell>{transaction.transactionDate}</TableCell>
-                                <TableCell>{transaction.postDate}</TableCell>
+                                <TableCell>{transaction.account.institution.name}<br />{transaction.account.account}</TableCell>
+                                <TableCell>{moment(transaction.transactionDate).format('M/D/YY')}</TableCell>
                                 <TableCell>{transaction.description}</TableCell>
-                                <TableCell>{transaction.amount}</TableCell>
-                                <TableCell>{transaction.budgetCategory ? `${transaction.budgetCategory.budgetGroup.name}::${transaction.budgetCategory.name}`: ''}</TableCell>
-                                <TableCell>{`${transaction.reimbursable}`}</TableCell>
-                                <TableCell>{`${transaction.reimbursement}`}</TableCell>
-                                <TableCell>{`${transaction.payment}`}</TableCell>
+                                <TableCell align="right">{formatter.format(transaction.amount)}</TableCell>
+                                <TableCell>{transaction.budgetCategory ? (<>{transaction.budgetCategory.budgetGroup.name}<br />{transaction.budgetCategory.name}</>) : ''}</TableCell>
                                 <TableCell>{transaction.comment}</TableCell>
+                                <TableCell><TransactionIcon type={getType(transaction)} /></TableCell>
+                                <TableCell><EditIcon color="disabled" /></TableCell>
                             </TableRow>
                         ))}
                     </TableBody>
