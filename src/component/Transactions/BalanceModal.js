31f3108d2be870e76d32a8b5602f163e62da0a46
Mitchell Richeson <mricheson@gmail.com>
2020-04-26 13:46:04 -0400

    Account Balances for the Month

--- src/component/Transactions/BalanceModal.js
+++ src/component/Transactions/BalanceModal.js
@@ -1,5 +1,9 @@
-import React from 'react';
-import { Slide, Dialog, DialogTitle, DialogContent, useTheme, makeStyles } from '@material-ui/core';
+import React, { useContext } from 'react';
+import { Slide, Dialog, DialogTitle, DialogContent, useTheme, makeStyles, List, ListItem, ListItemText } from '@material-ui/core';
+import { TransactionStoreContext } from '../../store/TransactionStore';
+import red from '@material-ui/core/colors/red';
+import green from '@material-ui/core/colors/green';
+import clsx from 'clsx';
 
 const useStyle = makeStyles(theme => ({
     root: {
@@ -7,6 +11,17 @@ const useStyle = makeStyles(theme => ({
     },
     formControl: {
         margin: [[theme.spacing(1), 0, theme.spacing(1), 0]],
+    },
+    income: {
+        color: green[900]
+    },
+    expense: {
+        color: red[900]
+    },
+    balance: {
+        '& span': {
+            textAlign: 'right'
+        }
     }
 }));
 
@@ -14,9 +29,23 @@ const Transition = React.forwardRef(function Transition(props, ref) {
     return <Slide direction="up" ref={ref} {...props} />;
 });
 
+const formatter = new Intl.NumberFormat('en-US', {
+    style: 'currency',
+    currency: 'USD',
+});
+
 const BalanceModal = ({ onSave, canSave = true, onClose, isSaving, title, mode = "edit", children }) => {
     const theme = useTheme();
     const classes = useStyle();
+
+    const transactionStore = useContext(TransactionStoreContext);
+
+    const balances = transactionStore.transactions.reduce((balances, transaction) => {
+        const account = balances[transaction.account.id] || { account: transaction.account, balance: 0 };
+        account.balance += transaction.amount;
+        balances[transaction.account.id] = account;
+        return balances;
+    }, {});
 
 
 
@@ -32,6 +61,16 @@ const BalanceModal = ({ onSave, canSave = true, onClose, isSaving, title, mode =
         >
             <DialogTitle>Month's Balances</DialogTitle>
             <DialogContent>
+                <List>
+                    {
+                        Object.values(balances).map(accountBalance => (
+                            <ListItem>
+                                <ListItemText primary={accountBalance.account.name} secondary={accountBalance.account.institution.name} />
+                                <ListItemText primary={formatter.format(Math.abs(accountBalance.balance))} className={clsx(classes.balance, accountBalance.balance.toFixed(2) > 0 ? classes.income : null, accountBalance.balance.toFixed(2) < 0 ? classes.expense : null)} />
+                            </ListItem>
+                        ))
+                    }
+                </List>
             </DialogContent>
         </Dialog>
     );
